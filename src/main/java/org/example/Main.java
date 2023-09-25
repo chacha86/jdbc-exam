c1adfe0da7a4b8cfb1ea454d3cb86e5d50201132
chacha86 <taejincha0619@gmail.com>
2023-09-25 11:28:43 +0900

    DB에서 데이터 꺼내오기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         // 자동임포트 : alt + enter
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
+        Connection conn = null; // DB 접속하는 객체
+        Statement stmt = null; // SQL 전송하는 객체
+        ResultSet rs = null; // 결과 받아오는 객체
 
         String url = "jdbc:mysql://localhost:3306/board?serverTimezone=UTC";
         String user = "root";
@@ -28,12 +28,29 @@ public class Main {
             //3. Statement 생성
             stmt = conn.createStatement();
 
-            System.out.println("3번까지 문제 없이 실행");
+//            System.out.println("3번까지 문제 없이 실행");
 
-//            //4. SQL 처리하고 결과 ResultSet에 받아오기
-//            String sql = "SELECT * FROM article";
-//            rs = stmt.executeQuery(sql);
-//
+            //4. SQL 처리하고 결과 ResultSet에 받아오기
+            String sql = "SELECT * FROM article";
+            rs = stmt.executeQuery(sql);
+
+            rs.next(); // 화살표를 한칸 내림.
+
+            // 첫번재 행의 id 꺼내오기
+            int id = rs.getInt("id");
+            System.out.println("첫번째 행의 id : " + id);
+
+            // 첫번째 행의 title 꺼내오기.
+            String title = rs.getString("title");
+            System.out.println("첫번째 행의 title : " + title);
+
+            // 두번째 행의 title 꺼내오기.
+            rs.next(); // 화살표 한칸 내림
+            String title2 = rs.getString("title");
+            System.out.println("두번째 행의 title : " + title2);
+
+            // 세번째 행의 content 꺼내오기
+
 //            while(rs.next()) {
 //                System.out.println(rs.getString("title"));
 //                System.out.println(rs.getString("content")); // 문자열로 리턴
