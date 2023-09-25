9e9d8ff69dc471c80d061b8b379ec3c359050e2c
chacha86 <taejincha0619@gmail.com>
2023-09-25 12:18:23 +0900

    DB에 데이터 반영하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,29 +34,12 @@ public class Main {
             String sql = "SELECT * FROM article";
             rs = stmt.executeQuery(sql);
 
-            rs.next(); // 화살표를 한칸 내림.
-
-            // 첫번재 행의 id 꺼내오기
-            int id = rs.getInt("id");
-            System.out.println("첫번째 행의 id : " + id);
-
-            // 첫번째 행의 title 꺼내오기.
-            String title = rs.getString("title");
-            System.out.println("첫번째 행의 title : " + title);
-
-            // 두번째 행의 title 꺼내오기.
-            rs.next(); // 화살표 한칸 내림
-            String title2 = rs.getString("title");
-            System.out.println("두번째 행의 title : " + title2);
-
-            // 세번째 행의 content 꺼내오기
-
-//            while(rs.next()) {
-//                System.out.println(rs.getString("title"));
-//                System.out.println(rs.getString("content")); // 문자열로 리턴
-//                System.out.println(rs.getInt("id")); // 문자열로 리턴
-//                System.out.println("========================");
-//            }
+            while(rs.next()) {
+                System.out.println(rs.getString("title"));
+                System.out.println(rs.getString("content")); // 문자열로 리턴
+                System.out.println(rs.getInt("id")); //
+                System.out.println("========================");
+            }
 
         } catch(Exception e) {
             System.out.println("접속 시도중 문제 발생!!");
